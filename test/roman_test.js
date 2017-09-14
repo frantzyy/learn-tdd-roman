8761278853a3640cde15ce7ced38f9972a5ce610
christopher frantz <christopher.frantz@libertymutual.com>
2017-09-13 22:28:47 -0400

    confirmed assert, expect, should are setup

--- test/roman_test.js
+++ test/roman_test.js
@@ -1,14 +1,30 @@
-import { assert } from 'chai';
+import * as chai from 'chai';
+
+let assert = chai.assert;
+let should = chai.should();
+let expect = chai.expect;
+
 import { name } from '../src/roman';
 
 
 describe('confirm setup', function() {
-    // Further code for tests goes here
 
-    it('should start empty', function() {
+    it('should have assert available for tdd', function() {
         var arr = [];
 
         assert.equal(arr.length, 0);
+    });
+
+    it('should have expect available for bdd', function() {
+        var arr = [];
+
+        expect(arr.length).to.equal(0);
+    });
+
+    it('should have should available for bdd', function() {
+        var arr = [];
+
+        arr.should.have.lengthOf(0);
     });
 
 });
